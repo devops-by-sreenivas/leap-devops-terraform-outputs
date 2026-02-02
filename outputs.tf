1820879200915389942252f1f00dd9eb071c7a2a
sreenivasulu_ramanaboina <sreeni.ramanaboina@gmail.com>
2026-02-02 17:16:18 -0600

    Create outputs.tf with applicant information
    Add Terraform outputs for applicant details

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+output "name" {
+  description = "Applicant name"
+  value       = "Sreenivasulu Ramanaboina"
+}
+
+output "position_title" {
+  description = "Position being applied for"
+  value       = "Senior DevOps Engineer"
+}
